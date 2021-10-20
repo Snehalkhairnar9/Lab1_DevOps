3b369fc63383888818923c58b5076033da8c4a07
Snehalkhairnar9 <L00163434@student.lyit.ie>
2021-10-20 16:00:56 +0100

    Python_code_test

---
+++ addition.py
@@ -0,0 +1,16 @@
+# File          : addition.py
+# Author        : Snehal Khairnar
+# Version       : v1.0
+# Created Date  : 20/10/2021
+# Description   : Testing the addition of two numbers
+# Licensing     : Snehal Khairnar, LYIT
+# ----------------------------------
+
+
+a = 50
+b = 60
+c = a + b
+if c == 110:
+  print("Test passed")
+else:
+  print("test failed")
